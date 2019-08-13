fcfef1ed4b14b91bc0f1bfd9799ee589de707bb1
Matt Maclean <mwmaclean88@gmail.com>
2019-08-13 16:00:49 +0100

    eslint fixes & yarn upgrade

--- src/components/FormSteps/StepFiles.vue
+++ src/components/FormSteps/StepFiles.vue
@@ -1,92 +1,77 @@
 <template>
-    <v-form ref="formstep2">
-        <v-flex
-            v-for="(input, key, index) in formInputs.step2"
-            :key="index"
-            xs12
-            ma-3
-        >
-            <v-btn
-                v-if="input.type === 'file'"
-                @click="openFileDialog(key)"
-            >
-                {{ input.btnlabel }}
+  <v-form ref="formstep2">
+    <v-flex v-for="(input, key, index) in formInputs.step2" :key="index" xs12 ma-3>
+      <v-btn v-if="input.type === 'file'" @click="openFileDialog(key)">
+        {{ input.btnlabel }}
+        <v-icon right dark>cloud_upload</v-icon>
+      </v-btn>
+      <input
+        :ref="key"
+        type="file"
+        style="display:none"
+        :rules="input.validationRules"
+        :required="input.required"
+        @change="onFileChange(key, $event)"
+      />
 
-                <v-icon right dark>
-                    cloud_upload
-                </v-icon>
-            </v-btn>
-            <input
-                :ref="key"
-                type="file"
-                style="display:none"
-                :rules="input.validationRules"
-                :required="input.required"
-                @change="onFileChange(key, $event)"
-            >
+      <v-spacer />
+    </v-flex>
 
-            <v-spacer />
-        </v-flex>
+    <v-spacer />
 
-        <v-spacer />
+    <v-flex v-if="uploadedFiles" xs12 ma-3>
+      <v-chip v-for="(file, index) in uploadedFiles" :key="index">
+        <v-avatar class="teal">
+          <v-icon>attachment</v-icon>
+        </v-avatar>
+        File Uploaded: {{ file.value.name }}
+      </v-chip>
+    </v-flex>
 
-        <v-flex v-if="uploadedFiles" xs12 ma-3>
-            <v-chip v-for="(file, index) in uploadedFiles" :key="index">
-                <v-avatar class="teal">
-                    <v-icon>attachment</v-icon>
-                </v-avatar>
-                File Uploaded: {{ file.value.name }}
-            </v-chip>
-        </v-flex>
+    <v-btn color="primary" @click="validateStep">Continue</v-btn>
 
-        <v-btn color="primary" @click="validateStep">
-            Continue
-        </v-btn>
+    <v-btn flat @click="$emit('previousStep')">Back</v-btn>
 
-        <v-btn flat @click="$emit('previousStep')">
-            Back
-        </v-btn>
+    <v-dialog v-model="showDialog" width="500">
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>
+          <h3>Upload a CV</h3>
+        </v-card-title>
 
-        <v-dialog v-model="showDialog" width="500">
-            <v-card>
-                <v-card-title class="headline grey lighten-2" primary-title>
-                    <h3>Upload a CV</h3>
-                </v-card-title>
-
-                <v-card-text>
-                    <p>You must upload a CV file to continue</p>
-                    <v-spacer />
-                    <v-btn @click="showDialog = false">
-                        OK
-                    </v-btn>
-                </v-card-text>
-            </v-card>
-        </v-dialog>
-    </v-form>
+        <v-card-text>
+          <p>You must upload a CV file to continue</p>
+          <v-spacer />
+          <v-btn @click="showDialog = false">OK</v-btn>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </v-form>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
 
 export default {
-  name: 'step-files',
+  name: 'step-files"',
   data: () => ({
-    showDialog: false,
+    showDialog: false
   }),
   computed: {
     ...mapState({
-      formInputs: 'formInputs',
+      formInputs: 'formInputs'
     }),
     uploadedFiles() {
-        return Object.values(this.formInputs.step2).filter(files => Boolean(files.value));
+      return Object.values(this.formInputs.step2).filter(files =>
+        Boolean(files.value)
+      );
     },
     hasUploadedCV() {
-        return this.uploadedFiles.some(files => files.label === 'CV');
-    },
+      return this.uploadedFiles.some(files => files.label === 'CV');
+    }
   },
   methods: {
     ...mapActions({
-        updateVal: 'triggerUpdateField',
+      updateVal: 'triggerUpdateField'
     }),
     validateStep() {
       if (!this.hasUploadedCV) {
@@ -96,16 +81,16 @@ export default {
       }
     },
     openFileDialog(inputRef) {
-        this.$refs[inputRef][0].click();
+      this.$refs[inputRef][0].click();
     },
     onFileChange(inputKey, e) {
-        const fileUploaded = e.target.files || e.dataTransfer.files;
-        const fileCount = fileUploaded.length;
-        if (fileCount > 0) {
-            this.updateVal(['step2', inputKey, fileUploaded[0]]);
-        }
-    },
-  },
+      const fileUploaded = e.target.files || e.dataTransfer.files;
+      const fileCount = fileUploaded.length;
+      if (fileCount > 0) {
+        this.updateVal(['step2', inputKey, fileUploaded[0]]);
+      }
+    }
+  }
 };
 </script>
 
--- src/components/FormSteps/StepPersonal.vue
+++ src/components/FormSteps/StepPersonal.vue
@@ -1,67 +1,56 @@
 <template>
-    <v-form
-        ref="formstep1"
-        v-model="valid"
-    >
-        <v-container>
-            <v-layout row wrap>
-                <v-flex
-                    v-for="(input, key, index) in formInputs.step1"
-                    :key="index"
-                    xs12
-                    sm6
-                >
-                    <v-text-field
-                        v-if="input.type === 'text'"
-                        v-model.lazy="input.value"
-                        :label="input.label"
-                        :required="input.required"
-                        :rules="input.validationRules"
-                        @input="updateVal(['step1', key, input.value])"
-                    />
-                    <v-checkbox
-                        v-if="input.type === 'checkbox'"
-                        v-model="input.value"
-                        :label="input.label"
-                        :required="input.required"
-                        :rules="input.validationRules"
-                        @change="updateVal(['step1', key, input.value])"
-                    />
-                </v-flex>
-            </v-layout>
-        </v-container>
-        <v-btn color="primary" @click="validateStep">
-            Continue
-        </v-btn>
-    </v-form>
+  <v-form ref="formstep1" v-model="valid">
+    <v-container>
+      <v-layout row wrap>
+        <v-flex v-for="(input, key, index) in formInputs.step1" :key="index" xs12 sm6>
+          <v-text-field
+            v-if="input.type === 'text'"
+            v-model.lazy="input.value"
+            :label="input.label"
+            :required="input.required"
+            :rules="input.validationRules"
+            @input="updateVal(['step1', key, input.value])"
+          />
+          <v-checkbox
+            v-if="input.type === 'checkbox'"
+            v-model="input.value"
+            :label="input.label"
+            :required="input.required"
+            :rules="input.validationRules"
+            @change="updateVal(['step1', key, input.value])"
+          />
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-btn color="primary" @click="validateStep">Continue</v-btn>
+  </v-form>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
 
 export default {
-    name: 'step-personal',
-    data: () => ({
-        valid: false,
+  name: 'step-personal',
+  data: () => ({
+    valid: false
+  }),
+  computed: {
+    ...mapState({
+      formInputs: 'formInputs'
+    })
+  },
+  methods: {
+    ...mapActions({
+      updateVal: 'triggerUpdateField'
     }),
-    computed: {
-        ...mapState({
-            formInputs: 'formInputs',
-        }),
-    },
-    methods: {
-        ...mapActions({
-            updateVal: 'triggerUpdateField',
-        }),
-        validateStep() {
-            if (this.$refs.formstep1.validate()) {
-                this.$emit('nextStep');
-            }
-        },
-    },
+    validateStep() {
+      if (this.$refs.formstep1.validate()) {
+        this.$emit('nextStep');
+      }
+    }
+  }
 };
 </script>
 
 <style>
-
 </style>
